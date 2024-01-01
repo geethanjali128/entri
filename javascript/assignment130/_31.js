32899a02eae32e17659dbed42b10c2447eb0c10c
geethanjali128 <karrageethanjali2003@gmail.com>
2024-01-01 22:28:19 +0530

    javascript

---
+++ javascript/assignment130/_31.js
@@ -0,0 +1,14 @@
+// 31. Write a function `findIndex` that takes an array and an element as input and returns the index of the first occurrence of the element in the array. If the element is not found, return -1.
+
+// 1)
+
+function findIndex(arr,element){
+    return arr.indexOf(element)
+}
+
+// const inputArray = [1, 2, 3, 4, 5];
+//     const element = 3;
+//     const output = findIndex(inputArray, element);
+//     console.log(output); // 2
+
+
