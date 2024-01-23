b53d5770a88289ba8a5035052f1a192646b9f189
geethanjali128 <karrageethanjali2003@gmail.com>
2024-01-23 22:13:20 +0530

    javascript

---
+++ javascript/assignment130/_48.js
@@ -0,0 +1,9 @@
+// **4. Print Multiplication Table:**
+// Write a program to print the multiplication table of a given number `n` using a `for` loop.
+
+const n="hello"
+let reversed=" "
+for(let i=n.length-1;i>=0;i--){
+    reversed+=n[i]
+}
+console.log(reversed)
