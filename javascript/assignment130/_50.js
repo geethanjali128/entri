6c61239872706c629d01ad47df6b02f279744253
geethanjali128 <karrageethanjali2003@gmail.com>
2024-01-24 22:50:11 +0530

    javascript

---
+++ javascript/assignment130/_50.js
@@ -0,0 +1,17 @@
+
+// **7. Count Digits in a Number:**
+// Write a program to count the number of digits in a given number `n` using a `for` loop.
+
+const n=12345
+let count=0
+// while(n!==0){
+//     count++
+    
+// }
+for(let i=0;i<=n.length;i++){
+    if(n!==0){
+        count++
+        // Math.floor(n/10)
+    }
+}
+console.log(count)
